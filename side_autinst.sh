4443ccd8d92ea739bdc3e187d29749edc1e330b8
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2024-05-07 13:18:23 +0300

    Update side_autinst.sh

--- side_autinst.sh
+++ side_autinst.sh
@@ -21,27 +21,29 @@ sudo apt install -y curl git jq lz4 build-essential
 
 # Install Go
 sudo rm -rf /usr/local/go
-curl -L https://go.dev/dl/go1.21.6.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
-echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
+curl -Ls https://go.dev/dl/go1.21.9.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
+eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
 source .bash_profile
+source $HOME/.profile
 
 # Clone project repository
 systemctl stop sided
 cd && rm -rf sidechain
 rm -rf .side/data
-mkdir .side2-bak
-cp -r .side .side2-bak
+mkdir .side3-bak
+cp -r .side .side3-bak
 rm -rf .side
 
 git clone https://github.com/sideprotocol/sidechain.git
 cd sidechain
-git checkout v0.7.0-rc2
+git checkout v0.8.1
 
 # Build binary
 make install
 
 # Set node CLI configuration
-sided config chain-id side-testnet-3
+sided config chain-id S2-testnet-2
 sided config keyring-backend test
 sided config node tcp://localhost:26357
 
@@ -49,20 +51,21 @@ sided config node tcp://localhost:26357
 sided init $SIDE_MONIK --chain-id side-testnet-3
 
 # Download genesis and addrbook files
-curl -Ls https://snapshots.indonode.net/side-t/genesis.json > $HOME/.side/config/genesis.json
-curl -Ls https://snapshots.indonode.net/side-t/addrbook.json > $HOME/.side/config/addrbook.json
+curl -Ls https://snapshots.kjnodes.com/side-testnet/genesis.json > $HOME/.side/config/genesis.json
+curl -Ls https://snapshots.kjnodes.com/side-testnet/addrbook.json > $HOME/.side/config/addrbook.json
 
 # Set seeds
-sed -i -e 's|^seeds *=.*|seeds = "6decdc5565bf5232cdf5597a7784bfe828c32277@158.220.126.137:11656,e9ee4fb923d5aab89207df36ce660ff1b882fc72@136.243.33.177:21656,9c14080752bdfa33f4624f83cd155e2d3976e303@side-testnet-seed.itrocket.net:45656"|' $HOME/.side/config/config.toml
+sed -i -e "s|^seeds *=.*|seeds = \"3f472746f46493309650e5a033076689996c8881@side-testnet.rpc.kjnodes.com:17459\"|" $HOME/.side/config/config.toml
 
 # Set minimum gas price
-sed -i -e 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.005uside"|' $HOME/.side/config/app.toml
+sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.005uside\"|" $HOME/.side/config/app.toml
 
 # Set pruning
 sed -i \
   -e 's|^pruning *=.*|pruning = "custom"|' \
   -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
-  -e 's|^pruning-interval *=.*|pruning-interval = "17"|' \
+  -e 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|' \
+  -e 's|^pruning-interval *=.*|pruning-interval = "19"|' \
   $HOME/.side/config/app.toml
 
 # Change ports
@@ -70,7 +73,7 @@ sed -i -e "s%:1317%:26317%; s%:8080%:26380%; s%:9090%:26390%; s%:9091%:26391%; s
 sed -i -e "s%:26658%:26358%; s%:26657%:26357%; s%:6060%:26360%; s%:26656%:26356%; s%:26660%:26361%" $HOME/.side/config/config.toml
 
 
-curl -L https://snapshots.indonode.net/side-t/side-t-latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.side
+curl -L https://snapshots.kjnodes.com/side-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.side
 
 # Create a service
 sudo tee /etc/systemd/system/sided.service > /dev/null << EOF
