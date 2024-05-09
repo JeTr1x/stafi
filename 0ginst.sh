d9419ab04a5237024d5cff856c1e194d53addb02
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2024-05-09 12:58:24 +0300

    Update 0ginst.sh

--- 0ginst.sh
+++ 0ginst.sh
@@ -14,49 +14,65 @@ echo -e "\e[0m"
 sleep 1
 read -p "Enter node name: " ZG_MONIK
 echo 'export ZG_MONIK='$ZG_MONIK >> $HOME/.bash_profile
-CHAIN_ID="zgtendermint_9000-1"
+read -p "Enter portnum (10-64): " ZGP_PORT
+echo 'export ZGP_PORT='$ZGP_PORT >> $HOME/.bash_profile
+CHAIN_ID="zgtendermint_16600-1"
 
 sudo apt update
 sudo apt install curl git jq build-essential gcc unzip wget lz4 -y
 
+cd $HOME
+rm -rf 0g-chain
+git clone -b v0.1.0 https://github.com/0glabs/0g-chain.git
+./0g-chain/networks/testnet/install.sh
+source .profile
 
 
-git clone https://github.com/0glabs/0g-evmos.git
-cd 0g-evmos
-git checkout v1.0.0-testnet
-wget http://159.69.72.247:13413/evmosd-og.tar.gz
-tar xvzf evmosd-og.tar.gz
-chmod +x evmosd-og
-cp evmosd-og /root/go/bin/evmosd
-evmosd version
 sleep 1
 
 cd $HOME
-evmosd init $ZG_MONIK --chain-id $CHAIN_ID
-evmosd config chain-id $CHAIN_ID
-evmosd config node tcp://localhost:22357
-evmosd config keyring-backend test
+0gchaind init $ZG_MONIK --chain-id $CHAIN_ID
+0gchaind config chain-id $CHAIN_ID
+0gchaind config node tcp://localhost:22357
+0gchaind config keyring-backend test
 
 
 
-wget https://github.com/0glabs/0g-evmos/releases/download/v1.0.0-testnet/genesis.json -O $HOME/.evmosd/config/genesis.json
-PEERS="1248487ea585730cdf5d3c32e0c2a43ad0cda973@peer-zero-gravity-testnet.trusted-point.com:26326" && \
-SEEDS="8c01665f88896bca44e8902a30e4278bed08033f@54.241.167.190:26656,b288e8b37f4b0dbd9a03e8ce926cd9c801aacf27@54.176.175.48:26656,8e20e8e88d504e67c7a3a58c2ea31d965aa2a890@54.193.250.204:26656,e50ac888b35175bfd4f999697bdeb5b7b52bfc06@54.215.187.94:26656" && \
-sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.evmosd/config/config.toml
-
+wget -P ~/.0gchain/config https://github.com/0glabs/0g-chain/releases/download/v0.1.0/genesis.json
+PEERS="" && \
+SEEDS="c4d619f6088cb0b24b4ab43a0510bf9251ab5d7f@54.241.167.190:26656,44d11d4ba92a01b520923f51632d2450984d5886@54.176.175.48:26656,f2693dd86766b5bf8fd6ab87e2e970d564d20aff@54.193.250.204:26656,f878d40c538c8c23653a5b70f615f8dccec6fb9f@54.215.187.94:26656" && \
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.0gchain/config/config.toml
 # Set pruning
 sed -i \
   -e 's|^pruning *=.*|pruning = "custom"|' \
   -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
   -e 's|^pruning-interval *=.*|pruning-interval = "17"|' \
-  $HOME/.evmosd/config/app.toml
+  $HOME/.0gchain/config/app.toml
 
 # Change ports
-sed -i -e "s%:1317%:22317%; s%:8080%:22380%; s%:9090%:22390%; s%:9091%:22391%; s%:8545%:22345%; s%:8546%:22346%; s%:6065%:22365%" $HOME/.evmosd/config/app.toml
-sed -i -e "s%:26658%:22358%; s%:26657%:22357%; s%:6060%:22360%; s%:26656%:22356%; s%:26660%:22361%" $HOME/.evmosd/config/config.toml
 
-sed -i "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.00252aevmos\"/" $HOME/.evmosd/config/app.toml
+EXTERNAL_IP=$(wget -qO- eth0.me) \
+PROXY_APP_PORT=${ZGP_PORT}658 \
+P2P_PORT=${ZGP_PORT}656 \
+PPROF_PORT=${ZGP_PORT}060 \
+API_PORT=${ZGP_PORT}317 \
+GRPC_PORT=${ZGP_PORT}090 \
+GRPC_WEB_PORT=${ZGP_PORT}091
 
+sed -i \
+    -e "s/\(proxy_app = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$PROXY_APP_PORT\"/" \
+    -e "s/\(laddr = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$RPC_PORT\"/" \
+    -e "s/\(pprof_laddr = \"\)\([^:]*\):\([0-9]*\).*/\1localhost:$PPROF_PORT\"/" \
+    -e "/\[p2p\]/,/^\[/{s/\(laddr = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$P2P_PORT\"/}" \
+    -e "/\[p2p\]/,/^\[/{s/\(external_address = \"\)\([^:]*\):\([0-9]*\).*/\1${EXTERNAL_IP}:$P2P_PORT\"/; t; s/\(external_address = \"\).*/\1${EXTERNAL_IP}:$P2P_PORT\"/}" \
+    $HOME/.0gchain/config/config.toml
+sed -i \
+    -e "/\[api\]/,/^\[/{s/\(address = \"tcp:\/\/\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$API_PORT\4/}" \
+    -e "/\[grpc\]/,/^\[/{s/\(address = \"\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$GRPC_PORT\4/}" \
+    -e "/\[grpc-web\]/,/^\[/{s/\(address = \"\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$GRPC_WEB_PORT\4/}" $HOME/.0gchain/config/app.toml
+    
+sed -i -e "s%:8545%:22345%; s%:8546%:22346%; s%:6065%:22365%" $HOME/.evmosd/config/app.toml
+sed -i "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0ua0gi\"/" $HOME/.0gchain/config/app.toml
 
 sudo tee /etc/systemd/system/ogd.service > /dev/null <<EOF
 [Unit]
@@ -66,7 +82,7 @@ After=network.target
 [Service]
 User=$USER
 Type=simple
-ExecStart=$(which evmosd) start --home $HOME/.evmosd
+ExecStart=$(which 0gchaind) start --home $HOME/.0gchain
 Restart=on-failure
 LimitNOFILE=65535
 
