ab4aade41e1638c4e6431abf2d575f8c91663834
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2024-05-13 16:57:54 +0300

    Create 0g16600_np.sh

---
+++ 0g16600_np.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+echo -e "\033[1;36m"
+echo " ::::::'##:'########:'########:'########::'####:'##::::'## ";
+echo " :::::: ##: ##.....::... ##..:: ##.... ##:. ##::. ##::'## ";
+echo " :::::: ##: ##:::::::::: ##:::: ##:::: ##:: ##:::. ##'## ";
+echo " :::::: ##: ######:::::: ##:::: ########::: ##::::. ### ";
+echo " '##::: ##: ##...::::::: ##:::: ##.. ##:::: ##:::: ## ## ";
+echo "  ##::: ##: ##:::::::::: ##:::: ##::. ##::: ##::: ##:. ## ";
+echo " . ######:: ########:::: ##:::: ##:::. ##:'####: ##:::. ## ";
+echo " :......:::........:::::..:::::..:::::..::....::..:::::..::";
+echo -e "\e[0m"
+
+sleep 1
+read -p "Enter node name: " ZG_MONIK
+echo 'export ZG_MONIK='$ZG_MONIK >> $HOME/.bash_profile
+read -p "Enter portnum (10-64): " ZGP_PORT
+echo 'export ZGP_PORT='$ZGP_PORT >> $HOME/.bash_profile
+CHAIN_ID="zgtendermint_16600-1"
+
+sudo apt update
+sudo apt install curl git jq build-essential gcc unzip wget lz4 -y
+
+cd $HOME
+rm -rf 0g-chain
+git clone -b v0.1.0 https://github.com/0glabs/0g-chain.git
+./0g-chain/networks/testnet/install.sh
+source .profile
+
+
+sleep 1
+
+cd $HOME
+0gchaind init $ZG_MONIK --chain-id $CHAIN_ID
+0gchaind config chain-id $CHAIN_ID
+0gchaind config node tcp://localhost:${ZGP_PORT}657
+0gchaind config keyring-backend test
+
+
+rm ~/.0gchain/config/genesis.json
+wget -P ~/.0gchain/config https://github.com/0glabs/0g-chain/releases/download/v0.1.0/genesis.json
+curl -Ls https://snapshots.liveraven.net/snapshots/testnet/zero-gravity/addrbook.json > $HOME/.0gchain/config/addrbook.json
+PEERS="" && \
+SEEDS="c4d619f6088cb0b24b4ab43a0510bf9251ab5d7f@54.241.167.190:26656,44d11d4ba92a01b520923f51632d2450984d5886@54.176.175.48:26656,f2693dd86766b5bf8fd6ab87e2e970d564d20aff@54.193.250.204:26656,f878d40c538c8c23653a5b70f615f8dccec6fb9f@54.215.187.94:26656" && \
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.0gchain/config/config.toml
+# Set pruning
+sed -i \
+  -e 's|^pruning *=.*|pruning = "custom"|' \
+  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
+  -e 's|^pruning-interval *=.*|pruning-interval = "17"|' \
+  $HOME/.0gchain/config/app.toml
+
+# Change ports
+
+EXTERNAL_IP=$(wget -qO- eth0.me) \
+PROXY_APP_PORT=${ZGP_PORT}658 \
+P2P_PORT=${ZGP_PORT}656 \
+RPC_PORT=${ZGP_PORT}657 \
+PPROF_PORT=${ZGP_PORT}060 \
+API_PORT=${ZGP_PORT}317 \
+GRPC_PORT=${ZGP_PORT}090 \
+GRPC_WEB_PORT=${ZGP_PORT}091
+
+sed -i \
+    -e "s/\(proxy_app = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$PROXY_APP_PORT\"/" \
+    -e "s/\(laddr = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$RPC_PORT\"/" \
+    -e "s/\(pprof_laddr = \"\)\([^:]*\):\([0-9]*\).*/\1localhost:$PPROF_PORT\"/" \
+    -e "/\[p2p\]/,/^\[/{s/\(laddr = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$P2P_PORT\"/}" \
+    -e "/\[p2p\]/,/^\[/{s/\(external_address = \"\)\([^:]*\):\([0-9]*\).*/\1${EXTERNAL_IP}:$P2P_PORT\"/; t; s/\(external_address = \"\).*/\1${EXTERNAL_IP}:$P2P_PORT\"/}" \
+    $HOME/.0gchain/config/config.toml
+sed -i \
+    -e "/\[api\]/,/^\[/{s/\(address = \"tcp:\/\/\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$API_PORT\4/}" \
+    -e "/\[grpc\]/,/^\[/{s/\(address = \"\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$GRPC_PORT\4/}" \
+    -e "/\[grpc-web\]/,/^\[/{s/\(address = \"\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$GRPC_WEB_PORT\4/}" $HOME/.0gchain/config/app.toml
+    
+sed -i -e "s%:8545%:22345%; s%:8546%:22346%; s%:6065%:22365%" $HOME/.0gchain/config/app.toml
+sed -i "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0ua0gi\"/" $HOME/.0gchain/config/app.toml
+
+curl -L http://snapshots.liveraven.net/snapshots/testnet/zero-gravity/zgtendermint_16600-1_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.0gchain
+
+PEERS=$(curl -s --max-time 3 --retry 2 --retry-connrefused "https://snapshots.liveraven.net/snapshots/testnet/zero-gravity/peers.txt")
+if [ -z "$PEERS" ]; then
+    echo "No peers were retrieved from the URL."
+else
+    echo -e "\nPEERS: "$PEERS""
+    sed -i "s/^persistent_peers *=.*/persistent_peers = "$PEERS"/" "$HOME/.0gchain/config/config.toml"
+    echo -e "\nConfiguration file updated successfully.\n"
+fi
+
+
+sudo tee /etc/systemd/system/ogd.service > /dev/null <<EOF
+[Unit]
+Description=OG Node
+After=network.target
+
+[Service]
+User=$USER
+Type=simple
+ExecStart=$(which 0gchaind) start --home $HOME/.0gchain
+Restart=on-failure
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+
+sudo systemctl daemon-reload && \
+sudo systemctl enable ogd && \
+sudo systemctl restart ogd && \
+sudo journalctl -u ogd -f -o cat
