9173c2eb1fd4f0416286cc8869d8c417bd7f2170
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2024-05-22 16:25:56 +0300

    Update regdin.sh

--- regdin.sh
+++ regdin.sh
@@ -1,6 +1,7 @@
 
-sudo tee regdomain.sh >/dev/null <<EOF
+
 read -p "Enter domain: " INITIA_DOMAIN
+echo 'export INITIA_DOMAIN='$INITIA_DOMAIN >> $HOME/.bash_profile
 netstat -tulpn | grep 657
 read -p "Enter portnum (10-64): " INITIA_DPORT
 echo 'export INITIA_DPORT='$INITIA_DPORT >> $HOME/.bash_profile
