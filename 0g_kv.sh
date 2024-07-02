178e9d29ee488909ec0aa2819e46063b9fbb4616
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2024-07-02 10:42:25 +0300

    Update 0g_kv.sh

--- 0g_kv.sh
+++ 0g_kv.sh
@@ -1,5 +1,6 @@
 
-
+systemctl stop zgs_kv
+rm -rf 0g-storage-kv
 git clone https://github.com/0glabs/0g-storage-kv.git
 
 cd 0g-storage-kv
@@ -9,9 +10,9 @@ chmod +x zgs_kv
 mv zgs_kv $HOME/0g-storage-kv/target/release/
 cp $HOME/0g-storage-kv/run/config_example.toml $HOME/0g-storage-kv/run/config.toml
 
-LOG_CONTRACT_ADDRESS="0xb8F03061969da6Ad38f0a4a9f8a86bE71dA3c8E7"
-ZGS_LOG_SYNC_BLOCK="334797"
-BLOCKCHAIN_RPC_ENDPOINT="https://0gevmrpc.nodebrand.xyz"
+LOG_CONTRACT_ADDRESS="0x8873cc79c5b3b5666535C825205C9a128B1D75F1"
+ZGS_LOG_SYNC_BLOCK="802"
+BLOCKCHAIN_RPC_ENDPOINT="http://157.90.128.250:22345"
 
 
 sed -i '
@@ -19,3 +20,25 @@ s|^\s*#\?\s*log_contract_address\s*=.*|log_contract_address = "'"$LOG_CONTRACT_A
 s|^\s*#\?\s*log_sync_start_block_number\s*=.*|log_sync_start_block_number = '"$ZGS_LOG_SYNC_BLOCK"'|
 s|^\s*#\?\s*blockchain_rpc_endpoint\s*=.*|blockchain_rpc_endpoint = "'"$BLOCKCHAIN_RPC_ENDPOINT"'"|
 ' $HOME/0g-storage-kv/run/config.toml
+
+sudo tee /etc/systemd/system/zgs_kv.service > /dev/null <<EOF
+[Unit]
+Description=zgs_kv Node
+After=network.target
+
+[Service]
+User=root
+WorkingDirectory=/root/0g-storage-kv/run
+ExecStart=/root/0g-storage-kv/target/release/zgs_kv --config /home/ritual/0g-storage-kv/run/config.toml
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload && \
+sudo systemctl enable zgs_kv && \
+sudo systemctl start zgs_kv && \
+sudo journalctl -u zgs_kv -f -o cat
