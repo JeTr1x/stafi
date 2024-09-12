86be0928e13fe487d4ab744c71d22c16f6c593c4
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2024-09-12 18:49:55 +0300

    Update nwaku_inst.sh

--- nwaku_inst.sh
+++ nwaku_inst.sh
@@ -34,7 +34,7 @@ sed -i "s|8000:8000|8222:8000|g" docker-compose.yml
 sed -i "s|- 80:80 #Let's Encrypt|- 82:80 #Let's Encrypt|g" docker-compose.yml
 echo "Environment configuration completed!"
 
-SLPRND=$(echo $(( RANDOM % 36000 )))
+SLPRND=$(echo $(( RANDOM % 18000 )))
 echo "Sleeping 7 hours seconds and" $SLPRND "seconds after"
 sleep 25200
 sleep $SLPRND
