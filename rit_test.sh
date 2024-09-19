d29c030ee6809afa671a78ba202d6b315a621c6e
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2024-09-19 19:04:18 +0300

    Update rit_test.sh

--- rit_test.sh
+++ rit_test.sh
@@ -14,9 +14,8 @@ make build
 cd ~/infernet-container-starter/deploy
 sed -ie "s|ritualnetwork/infernet-node:latest|ritualnetwork/infernet-node:1.0.0|" docker-compose.yaml
 wget -O  config.json https://raw.githubusercontent.com/JeTr1x/stafi/main/rit_new_baseconf.json
-sed -ie "s|ritualnetwork/infernet-node:latest|ritualnetwork/infernet-node:1.0.0|" docker-compose.yaml
-sed -ie 's|"private_key": "",|"private_key": "${PRIVKEY}",|' config.json
-sed -ie 's|"https://base-rpc.publicnode.com"|'"$RPC_HTTP"'|' config.json
-sed -ie 's|"id": "hello-world",|"id": "${CTR_ID}",|' config.json
+sed -ie 's|"private_key": "",|"private_key": "'"$PRIVKEY"'",|' config.json
+sed -ie 's|"https://base-rpc.publicnode.com"|"'"$RPC_HTTP"'"|' config.json
+sed -ie 's|"id": "hello-world",|"id": "'"$CTR_ID"'",|' config.json
 
 docker compose up -d
