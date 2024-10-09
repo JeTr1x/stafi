1563dea4931fd46c114224f1c2734bd635ccef2e
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2024-10-09 17:53:14 +0300

    Create fuel_inst.sh

---
+++ fuel_inst.sh
@@ -0,0 +1,26 @@
+
+
+curl https://install.fuel.network | sh
+git clone https://github.com/FuelLabs/chain-configuration
+read -p "Enter P2P_PRIVATE_KEY: " P2P_PRIVATE_KEY
+
+sudo tee /etc/systemd/system/fueld.service > /dev/null <<EOF
+[Unit]
+Description=fuel full node
+After=network-online.target
+[Service]
+User=root
+WorkingDirectory=/root
+ExecStart=/root/.fuelup/bin/fuel-core run --service-name=fuel-sepolia-testnet-node --keypair $P2P_PRIVATE_KEY --relayer http://162.55.4.42:32545 --ip=0.0.0.0 --port=4500 --peering-port=36333 --db-path=~/.fuel-sepolia-testnet --snapshot /root/chain-configuration/ignition-test --utxo-validation --poa-instant false --enable-p2p --reserved-nodes /dns4/p2p-testnet.fuel.network/tcp/30333/p2p/16Uiu2HAmDxoChB7AheKNvCVpD4PHJwuDGn8rifMBEHmEynGHvHrf --sync-header-batch-size 100 --enable-relayer --relayer-v2-listening-contracts=0x01855B78C1f8868DE70e84507ec735983bf262dA --relayer-da-deploy-height=5827607 --relayer-log-page-size=500 --sync-block-stream-buffer-size 30
+Restart=always
+RestartSec=10
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+systemctl daemon-reload
+systemctl restart fueld
+sleep 8
+systemctl status  fueld
