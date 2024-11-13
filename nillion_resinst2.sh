40b0b098855bab6aa23201b364c6144578dc5cc9
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2024-11-13 20:47:49 +0300

    Create nillion_resinst2.sh

---
+++ nillion_resinst2.sh
@@ -0,0 +1,13 @@
+SLPRND=$(echo $(( RANDOM % 3600 )))
+SLPT=$(( 0 + $SLPRND ))
+echo "Sleeping " $SLPT "seconds"
+sleep $SLPT
+
+
+echo "Stopping container..."
+docker stop $(docker ps -a | grep nillion)
+echo "Removing container..."
+docker rm $(docker ps -a | grep nillion)
+
+echo "Starting new container..."
+docker run --name nillion_verifier -d --restart=always -v ./nillion/verifier:/var/tmp nillion/verifier:v1.0.1 verify --rpc-endpoint "http://65.109.124.86:49657"
