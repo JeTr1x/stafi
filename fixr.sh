e361348cecb59c5f257ac701b65e95bd43698882
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2024-12-15 18:57:00 +0300

    Create fixr.sh

---
+++ fixr.sh
@@ -0,0 +1,20 @@
+read -rp "Enter PRIVKEY: " PRIVKEY
+read -rp "Enter RPC: " RPC_HTTP
+read -rp "Enter Contract_ID: " CTR_ID
+read -rp "Enter Contract_Address: " CTR_ADDR
+
+docker compose down
+rm config.json docker-compose.yaml
+
+git clone https://github.com/ritual-net/infernet-container-starter
+docker pull ritualnetwork/hello-world-infernet:latest
+
+cd ~/infernet-container-starter/deploy
+wget -O  docker-compose.yaml https://raw.githubusercontent.com/JeTr1x/stafi/refs/heads/main/ritual-compose.yaml
+wget -O  config.json https://raw.githubusercontent.com/JeTr1x/stafi/main/rit_new_baseconf.json
+sed -ie 's|"private_key": "",|"private_key": "'"$PRIVKEY"'",|' config.json
+sed -ie 's|"https://base-rpc.publicnode.com"|"'"$RPC_HTTP"'"|' config.json
+sed -ie 's|"id": "hello-world",|"id": "'"$CTR_ID"'",|' config.json
+
+docker compose up -d
+docker logs -f  infernet-node
